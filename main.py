2d189c424b51f5d2ffc1b1f368c4ea5661a9de12
Aditya <ranjan.aditya2901@gmail.com>
2023-08-14 18:29:00 +0530

    Message

---
+++ main.py
@@ -0,0 +1,13 @@
+from distutils.log import debug
+import pickle
+from flask import Flask , render_template, request
+
+app = Flask(__name__)
+# model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
