b81d8c04f4fdd0e9f6416b6a511215b4c4026057
Aditya <ranjan.aditya2901@gmail.com>
2023-08-16 15:43:00 +0530

    Changes

--- app.py
+++ app.py
@@ -7,9 +7,25 @@ model = pickle.load(open('ml\climate\model.pkl','rb'))
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
+
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+
+@app.route('/weather_predict', methods=['GET','POST'])
+def weather_predict():
     prediction = model.predict([[0.1,15,28,10,0,0,0,1]])
     output = round(prediction[0], 20)
-    return render_template('index.html',prediction_text=f'{output}')
+    return render_template('predict.html',prediction_text=f'Max temperature is: {output}')
+
+@app.route('/feed')
+def feed():
+    return render_template('feed.html')
 
 # @app.route('/predict', methods=['GET','POST'])
 # def predict():
